cba3c06528d1ed804e5f46fa3b624e2046282531
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-01-23 16:51:03 +0100

    remove alias to diff

--- init-env.ps1
+++ init-env.ps1
@@ -7,3 +7,9 @@ $psDefaultParameterValues = @{ '*:encoding' = 'utf8' }
 #    Add directory where diff.exe is located to PATH:
 #
 $env:path = "$( split-path (where.exe git)[0])\..\usr\bin;$env:path"
+
+#
+#    Remove rather stupid PowerSehll alias named diff for compare-object
+#
+remove-item alias:diff -force
+
