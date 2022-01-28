980c4c418d56469b2476a7c05e70104bca033979
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-01-28 21:45:16 +0100

    add noNewLine to psDefaultParameterValue

--- init-env.ps1
+++ init-env.ps1
@@ -1,7 +1,10 @@
 #
 #    Make sure redirection operator works mostly normal:
 #
-$psDefaultParameterValues = @{ '*:encoding' = 'utf8' }
+$psDefaultParameterValues = @{
+  '*:encoding'          = 'utf8'
+  'out-file:noNewLine'  = $true
+}
 
 #
 #    Add directory where diff.exe is located to PATH:
@@ -12,4 +15,3 @@ $env:path = "$( split-path (where.exe git)[0])\..\usr\bin;$env:path"
 #    Remove rather stupid PowerSehll alias named diff for compare-object
 #
 remove-item alias:diff -force
-
